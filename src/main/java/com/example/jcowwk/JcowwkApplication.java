ae721faf81214229a30306553b3eaa16157b1052
jcowwk <jcowwk@gmail.com>
2025-02-15 14:39:27 +0900

    git inital commit

---
+++ src/main/java/com/example/jcowwk/JcowwkApplication.java
@@ -0,0 +1,13 @@
+package com.example.jcowwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcowwkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcowwkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jcowwk/JcowwkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jcowwk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcowwkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
