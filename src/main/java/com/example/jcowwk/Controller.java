83e8e5a091c87dcd522a16870badb1d4c267db10
jcowwk <jcowwk@gmail.com>
2025-04-21 22:39:13 +0900

    컨트롤러 메서드 추가

--- src/main/java/com/example/jcowwk/Controller.java
+++ src/main/java/com/example/jcowwk/Controller.java
@@ -12,4 +12,14 @@ public class Controller {
     public List<String> getBoards() {
         return List.of("게시글1", "게시글2", "게시글3");
     }
+
+    @GetMapping("/{boardId}")
+    public List<String> getDetail(@PathVariable Long boardId) {
+        return List.of("게시글1", "게시글2", "게시글3");
+    }
+
+    @GetMapping("/grid")
+    public List<String> getGrid() {
+        return List.of("게시글1", "게시글2", "게시글3");
+    }
 }
